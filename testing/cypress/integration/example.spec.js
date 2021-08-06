24a4002a7489acb045c0a7fddb764567e5400976
KarzanBotani <22057746+KarzanBotani@users.noreply.github.com>
2021-08-06 14:04:23 +0200

    Update example.spec.js

--- testing/cypress/integration/example.spec.js
+++ testing/cypress/integration/example.spec.js
@@ -5,9 +5,15 @@ describe('Example Setup', () => {
   let provider;
   
   before(() => {
+    // This is how you restore the DB so you can re-create accounts etc
     cy.request('GET', `${Cypress.env('backendUrl')}/restore-db`);
+    
+    // This is how you load the static data from the local fixture files.
     cy.provider().then(r => (provider = r));
     cy.customer().then(r => (customer = r));
+    
+    // This is how you, once logged in, disable all tooltips
+    cy.setTooltips();
   });
   
   it('visit dooli', () => {
