884caf912e95a739693043ffd8633b3f147bf162
KarzanBotani <22057746+KarzanBotani@users.noreply.github.com>
2021-08-06 13:09:52 +0200

    Update landing_page.spec.js

--- testing/cypress/integration/landing_page.spec.js
+++ testing/cypress/integration/landing_page.spec.js
@@ -3,10 +3,13 @@
 describe('My First Test', () => {
   it('visit dooli', () => {
     cy.visit('/');
+    cy.url().should('contain', '/');
   })
+  
   it('Here comes the first step', () => {
     expect(true).to.equal(true)
   })
+  
   it('Here comes the second step', () => {
     expect(true).to.equal(true)
   })
